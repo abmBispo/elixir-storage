f55b7d6478a9a3845f20a5989a972b266e8be542
Alan Borges <sr.alan.bispo@gmail.com>
2020-09-08 21:13:41 -0300

    adding ets tables as cache

--- test/elixir_storage/registry_test.exs
+++ test/elixir_storage/registry_test.exs
@@ -6,9 +6,9 @@ defmodule ElixirStorage.RegistryTest do
     Bucket
   }
 
-  setup do
-    registry = start_supervised!(Registry)
-    %{registry: registry}
+  setup(context) do
+    _ = start_supervised!({Registry, name: context.test})
+    %{registry: context.test}
   end
 
   test "spawns buckets", %{registry: registry} do
@@ -23,6 +23,7 @@ defmodule ElixirStorage.RegistryTest do
     Registry.create(registry, "shopping")
     {:ok, bucket} = Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+    _ = Registry.create(registry, "bogus")
     assert Registry.lookup(registry, "shopping") == :error
   end
 
@@ -30,6 +31,16 @@ defmodule ElixirStorage.RegistryTest do
     Registry.create(registry, "shopping")
     {:ok, bucket} = Registry.lookup(registry, "shopping")
     Agent.stop(bucket, :shutdown)
+    _ = Registry.create(registry, "bogus")
     assert Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "bucket can crash at any time", %{registry: registry} do
+    Registry.create(registry, "shopping")
+    {:ok, bucket} = Registry.lookup(registry, "shopping")
+    # Simulate a bucket crash by explicitly and synchronously shutting it down
+    Agent.stop(bucket, :shutdown)
+    # Now trying to call the dead process causes a :noproc exit
+    catch_exit(Bucket.put(bucket, "milk", 3))
   end
 end
