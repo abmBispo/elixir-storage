5f41f4c7a6f488f0c6c1e828f90ddb55b9e6b4aa
Alan Borges <sr.alan.bispo@gmail.com>
2020-09-11 18:35:06 -0300

    Finished server

--- apps/elixir_storage_server/lib/elixir_storage_server/command.ex
+++ apps/elixir_storage_server/lib/elixir_storage_server/command.ex
@@ -2,10 +2,75 @@ defmodule ElixirStorageServer.Command do
   @doc ~S"""
   Parses the given `line` into a command.
   ## Examples
-  iex> KVServer.Command.parse("CREATE shopping\r\n")
-  {:ok, {:create, "shopping"}}
+    iex> ElixirStorageServer.Command.parse("CREATE shopping\r\n")
+    {:ok, {:create, "shopping"}}
+
+    iex> ElixirStorageServer.Command.parse "CREATE shopping \r\n"
+    {:ok, {:create, "shopping"}}
+
+    iex> ElixirStorageServer.Command.parse "PUT shopping milk 1\r\n"
+    {:ok, {:put, "shopping", "milk", "1"}}
+
+    iex> ElixirStorageServer.Command.parse "GET shopping milk\r\n"
+    {:ok, {:get, "shopping", "milk"}}
+
+    iex> ElixirStorageServer.Command.parse "DELETE shopping eggs\r\n"
+    {:ok, {:delete, "shopping", "eggs"}}
+
+  Unknown commands or commands with the wrong number of
+  arguments return an error:
+
+    iex> ElixirStorageServer.Command.parse "UNKNOWN shopping eggs\r\n"
+    {:error, :unknown_command}
+
+    iex> ElixirStorageServer.Command.parse "GET shopping\r\n"
+    {:error, :unknown_command}
   """
-  def parse(_line) do
-    :not_implemented
+  def parse(line) do
+    case String.split(line) do
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
+      ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
+      ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
+      _ -> {:error, :unknown_command}
+    end
+  end
+
+  @doc """
+  Runs the given command.
+  """
+  def run(command)
+
+  def run({:create, bucket}) do
+    ElixirStorage.Registry.create(ElixirStorage.Registry, bucket)
+    {:ok, "OK\r\n"}
+  end
+
+  def run({:get, bucket, key}) do
+    lookup(bucket, fn pid ->
+      value = ElixirStorage.Bucket.get(pid, key)
+      {:ok, "#{value}\r\nOK\r\n"}
+    end)
+  end
+
+  def run({:put, bucket, key, value}) do
+    lookup(bucket, fn pid ->
+      ElixirStorage.Bucket.put(pid, key, value)
+      {:ok, "OK\r\n"}
+    end)
+  end
+
+  def run({:delete, bucket, key}) do
+    lookup(bucket, fn pid ->
+      ElixirStorage.Bucket.delete(pid, key)
+      {:ok, "OK\r\n"}
+    end)
+  end
+
+  defp lookup(bucket, callback) do
+    case ElixirStorage.Registry.lookup(ElixirStorage.Registry, bucket) do
+      {:ok, pid} -> callback.(pid)
+      :error -> {:error, :not_found}
+    end
   end
 end
--- apps/elixir_storage_server/test/elixir_storage_server_test.exs
+++ apps/elixir_storage_server/test/elixir_storage_server_test.exs
@@ -1,8 +1,4 @@
 defmodule ElixirStorageServerTest do
   use ExUnit.Case
   doctest ElixirStorageServer
-
-  test "greets the world" do
-    assert ElixirStorageServer.hello() == :world
-  end
 end
